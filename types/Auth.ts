2acf4c04561cfd2da8ad0a5222b850cd9a872b16
loulou123546 <louis.maillard@protonmail.com>
2025-10-11 18:38:12 +0200

    feat: add types for signup + confirm

--- types/Auth.ts
+++ types/Auth.ts
@@ -23,3 +23,38 @@ export const LoginResponse = z.object({
 	error: z.string().optional(),
 });
 export type LoginResponse = z.infer<typeof LoginResponse>;
+
+export const SignupRequest = z.object({
+	username: z.string(),
+	password: z.string(),
+	email: z.string().email(),
+	turnstileToken: z.string(),
+});
+export type SignupRequest = z.infer<typeof SignupRequest>;
+
+export const SignupResponse = z.object({
+	userId: z.string().optional(),
+	success: z
+		.object({
+			confirmed: z.boolean(),
+			session_id: z.string(),
+		})
+		.optional(),
+	continue: z
+		.object({
+			code_sent: z.boolean(),
+			code_sent_via: z.string(),
+			code_sent_to: z.string(),
+			session_id: z.string(),
+		})
+		.optional(),
+	error: z.string().optional(),
+});
+export type SignupResponse = z.infer<typeof SignupResponse>;
+
+export const ConfirmSignupRequest = z.object({
+	username: z.string(),
+	session_id: z.string(),
+	code: z.string(),
+});
+export type ConfirmSignupRequest = z.infer<typeof ConfirmSignupRequest>;
