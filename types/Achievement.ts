b348a937476296b80c5644209ca19263ec59ec82
loulou123546 <louis.maillard@protonmail.com>
2025-10-19 22:51:27 +0200

    feat: add Achievements

---
+++ types/Achievement.ts
@@ -0,0 +1,21 @@
+import { z } from "zod";
+
+export const Achievement = z.object({
+	storyId: z.string(),
+	id: z.string(),
+	title: z.string(),
+	description: z.string(),
+	public: z.boolean(),
+});
+export type Achievement = z.infer<typeof Achievement>;
+
+export const PlayerAchievement = z.object({
+	userId: z.string(),
+	storyId: z.string(),
+	id: z.string(),
+	title: z.string(),
+	description: z.string(),
+	public: z.boolean(),
+	firstEarned: z.string().datetime(),
+});
+export type PlayerAchievement = z.infer<typeof PlayerAchievement>;
